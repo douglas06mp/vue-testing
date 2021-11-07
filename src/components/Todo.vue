26f35fb92a5bb2176d2a3d6fc4d8741d3f02b450
douglas <douglas06mp@gmail.com>
2021-11-07 20:18:54 +0800

    create todo component

---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h2>Todo</h2>
+    <div v-for="todo in todos" :key="todo.id" data-test="todo">
+      {{ todo.task }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    todos: [{ id: 1, task: 'make coffee', completed: false }],
+  }),
+}
+</script>
