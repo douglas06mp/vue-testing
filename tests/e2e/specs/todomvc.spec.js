df8d36ac8e90cc65c1aac8690166f003d11b0898
douglas <douglas06mp@gmail.com>
2021-11-20 19:51:34 +0800

    refactor

--- tests/e2e/specs/todomvc.spec.js
+++ tests/e2e/specs/todomvc.spec.js
@@ -1,16 +1,27 @@
 describe('todo mvc', () => {
-  context('Case 1: Initial State', () => {
-    beforeEach(() => {
-      cy.visit('https://todomvc.com/examples/vue/')
-    })
+  const selector = {
+    main: '.main',
+    footer: '.footer',
+    todoItems: '.todo-list .todo',
+    newTodo: '.new-todo',
+    lastOne: '.todo-list .todo:last-child',
+  }
 
+  const TODO_ITEM_ONE = 'Item 1'
+  const TODO_ITEM_TWO = 'Item 2'
+
+  beforeEach(() => {
+    cy.visit('https://todomvc.com/examples/vue/')
+  })
+
+  context('Case 1: Initial State', () => {
     it('should starts with zero todo item', () => {
-      cy.get('.todo-list .todo').should('have.length', 0)
+      cy.get(selector.todoItems).should('have.length', 0)
     })
 
     it('should hide main and footer', () => {
-      cy.get('.main').should('not.be.visible')
-      cy.get('.footer').should('not.be.visible')
+      cy.get(selector.main).should('not.be.visible')
+      cy.get(selector.footer).should('not.be.visible')
     })
   })
 })
